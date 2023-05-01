6985a9f9346638321b22d0372ec767dbdfae056e
Vladimir Pouzanov <farcaller@gmail.com>
2023-05-01 15:48:55 +0000

    mariadb-operator/mariadb-operator: init at 0.11.0

---
+++ charts/mariadb-operator/mariadb-operator/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://mariadb-operator.github.io/mariadb-operator/";
+  chart = "mariadb-operator";
+  version = "0.11.0";
+  chartHash = "sha256-qNLPOUb5teU+OVUlDZB4mamci2MoWQh5XuT+Rzfpavc=";
+}
