25b7495bbd84ed8fd0e1832161cb8548b4bd41d7
Vladimir Pouzanov <farcaller@gmail.com>
2023-05-03 12:36:28 +0000

    jetstack/cert-manager: init at v1.11.1

---
+++ charts/jetstack/cert-manager/default.nix
@@ -0,0 +1,7 @@
+{
+  repo = "https://charts.jetstack.io/";
+  chart = "cert-manager";
+  version = "v1.11.1";
+  chartHash = "sha256-sFDzDT+g2yKtIcPECBstR2BqzSwVwSgpJ4oPmel/H80=";
+  bogusVersion = true;
+}
