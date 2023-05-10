17561356d4b614293bc0365535202eccbfdcbf11
Vladimir Pouzanov <farcaller@gmail.com>
2023-05-10 09:41:29 +0000

    victoriametrics/victoria-metrics-cluster: init at 0.9.61

---
+++ charts/victoriametrics/victoria-metrics-cluster/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://victoriametrics.github.io/helm-charts/";
+  chart = "victoria-metrics-cluster";
+  version = "0.9.61";
+  chartHash = "sha256-u8ntuGIWzYzCDPOTUiu34PsgYMIqksTubnVRsrB7Xmk=";
+}
