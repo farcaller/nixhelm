043f7765b6025bc65448996d881c32f749b7a350
Vladimir Pouzanov <farcaller@gmail.com>
2023-05-10 09:41:49 +0000

    victoriametrics/victoria-metrics-operator: init at 0.21.0

---
+++ charts/victoriametrics/victoria-metrics-operator/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://victoriametrics.github.io/helm-charts/";
+  chart = "victoria-metrics-operator";
+  version = "0.21.0";
+  chartHash = "sha256-CdB/IAUqbUvo2BaoErASbkPQpfoWkbYaw3wTfghY/AU=";
+}
