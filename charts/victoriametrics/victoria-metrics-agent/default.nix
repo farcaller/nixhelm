4524cd382831dc6eb517a37f05cce4ecf0d9be20
Vladimir Pouzanov <farcaller@gmail.com>
2023-05-10 09:42:00 +0000

    victoriametrics/victoria-metrics-agent: init at 0.8.37

---
+++ charts/victoriametrics/victoria-metrics-agent/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://victoriametrics.github.io/helm-charts/";
+  chart = "victoria-metrics-agent";
+  version = "0.8.37";
+  chartHash = "sha256-GvImB7AAj8pe5AQkStGExcsmz9H3I8lMnIiSez0zNTc=";
+}
