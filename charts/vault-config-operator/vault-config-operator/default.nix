5cdefdbad10c4f1ccd3205ecb5be3b127f394cb1
Vladimir Pouzanov <farcaller@gmail.com>
2023-05-13 12:36:05 +0200

    vault-config-operator/vault-config-operator: init at v0.8.13

---
+++ charts/vault-config-operator/vault-config-operator/default.nix
@@ -0,0 +1,7 @@
+{
+  repo = "https://redhat-cop.github.io/vault-config-operator/";
+  chart = "vault-config-operator";
+  version = "v0.8.13";
+  chartHash = "sha256-BBiR1Jt1kSyMyAOSgacRcpFWs530oB76x7JVlzmDB8Q=";
+  bogusVersion = true;
+}
