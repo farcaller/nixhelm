0b0c799ff94e0e13d602aa0a4b96aa7da122c43a
Vladimir Pouzanov <farcaller@gmail.com>
2023-06-16 18:19:40 +0000

    external-dns/external-dns: init at 1.13.0

---
+++ charts/external-dns/external-dns/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://kubernetes-sigs.github.io/external-dns/";
+  chart = "external-dns";
+  version = "1.13.0";
+  chartHash = "sha256-5mwChgm+wKt9ED708PZVuHNv65XGKMcGyCtxawAFaFM=";
+}
