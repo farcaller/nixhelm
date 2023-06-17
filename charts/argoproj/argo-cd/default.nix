2f4eb639bc09353620c642d96cbd500ff5ebd811
Vladimir Pouzanov <farcaller@gmail.com>
2023-06-17 08:55:44 +0000

    argoproj/argo-cd: update to 5.36.2

--- charts/argoproj/argo-cd/default.nix
+++ charts/argoproj/argo-cd/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://argoproj.github.io/argo-helm/";
   chart = "argo-cd";
-  version = "5.36.1";
-  chartHash = "sha256-Tv276LevNTMcp/wng93fW+VUGdH8ZcQJZvDE8S2FQ04=";
+  version = "5.36.2";
+  chartHash = "sha256-kiA0VpvAobVJhwrBJJdhKryC3QgpvBaPlf8H+1nbhJk=";
 }
