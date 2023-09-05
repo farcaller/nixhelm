09d133aa5a7888c72c4b3da4b91c0c5405d3ff69
Vladimir Pouzanov <farcaller@gmail.com>
2023-09-05 14:32:33 +0000

    prometheus-community/prometheus-blackbox-exporter: init at 8.3.0

---
+++ charts/prometheus-community/prometheus-blackbox-exporter/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://prometheus-community.github.io/helm-charts/";
+  chart = "prometheus-blackbox-exporter";
+  version = "8.3.0";
+  chartHash = "sha256-gHddWM2QW6hZ2nGwG5lpTx+7cJ1f1KXdEout6SYu/94=";
+}
