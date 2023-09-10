53bc2a8b3617bf733f80380bdac7ec2e06e57993
Marcus Ramberg <marcus@means.no>
2023-09-10 07:44:33 +0200

    prometheus-community/prometheus: init at 24.3.0 (#2)

---
+++ charts/prometheus-community/prometheus/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://prometheus-community.github.io/helm-charts/";
+  chart = "prometheus";
+  version = "24.3.0";
+  chartHash = "sha256-oLMIlmXGt0a6yrsAgEeL+mscnXNlswaWwqIhL1R92F8=";
+}
