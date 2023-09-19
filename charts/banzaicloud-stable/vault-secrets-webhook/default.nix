fe1e87f0f3018a83c2863e9dadd623b8654f547b
jakuzure <shin@posteo.jp>
2023-09-19 16:10:21 +0200

    banzaicloud-stable/vault-secrets-webhook: init at 1.19.0

---
+++ charts/banzaicloud-stable/vault-secrets-webhook/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://kubernetes-charts.banzaicloud.com/";
+  chart = "vault-secrets-webhook";
+  version = "1.19.0";
+  chartHash = "sha256-q/FGa1C8eeWCOY5fPI0XXVihmIA9ExW4pP7viF823Ps=";
+}
