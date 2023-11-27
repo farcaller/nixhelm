35bd99ea52162d02c95ecd17320923c82e434922
Antonio Gurgel <antonio@goorzhel.com>
2023-11-27 11:51:59 -0800

    kubernetes-sigs/node-feature-discovery: init at 0.14.3

---
+++ charts/kubernetes-sigs/node-feature-discovery/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://kubernetes-sigs.github.io/node-feature-discovery/charts/";
+  chart = "node-feature-discovery";
+  version = "0.14.3";
+  chartHash = "sha256-4/FGHLCK1Kcs9tFs0EL5cZt3NLNeswtms+ZGPs+ju2k=";
+}
