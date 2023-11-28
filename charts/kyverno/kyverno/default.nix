8730625e6cec58d8aca523abebc4c7f9c3dbe208
Vladimir Pouzanov <farcaller@gmail.com>
2023-11-28 13:17:29 +0000

    Fixup the kyverno chart bogus version

--- charts/kyverno/kyverno/default.nix
+++ charts/kyverno/kyverno/default.nix
@@ -1,6 +1,7 @@
 {
   repo = "https://kyverno.github.io/kyverno";
   chart = "kyverno";
-  version = "3.1.0";
+  version = "v3.1.0";
+  bogusVersion = true;
   chartHash = "sha256-fu+BN/PWwIXumu/RtDZ2DyZs8r35o1ooGAFjHwDP104=";
 }
