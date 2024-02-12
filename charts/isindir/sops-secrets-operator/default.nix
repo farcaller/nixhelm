a4eaf4de1b4bf87afdfbee8c5bcee691f4b301bc
Aycan Irican <aycan@netskrt.io>
2024-02-12 10:23:21 -0800

    isindir/sops-secrets-operator: init at 0.18.3

---
+++ charts/isindir/sops-secrets-operator/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://isindir.github.io/sops-secrets-operator/";
+  chart = "sops-secrets-operator";
+  version = "0.18.3";
+  chartHash = "sha256-9lSOqBOAvgBEuNlQG2ffnG/6EkwPJKoeIBOoDWjsTQc=";
+}
