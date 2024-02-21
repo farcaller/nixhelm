14eb8426c2fa2b233a8ba715fcfbea2f9ac0696a
Vladimir Pouzanov <farcaller@gmail.com>
2024-02-21 21:32:59 +0000

    helmupdater: ignore versions it can't parse as semver

--- charts/rook-release/rook-ceph-cluster/default.nix
+++ charts/rook-release/rook-ceph-cluster/default.nix
@@ -3,4 +3,5 @@
   chart = "rook-ceph-cluster";
   version = "1.13.4";
   chartHash = "sha256-Hnpp9dE1eHl841Wef6Jfx1Zm2X58yUL5OvhLPg2h7Zk=";
+  bogusVersion = true;
 }
--- charts/rook-release/rook-ceph/default.nix
+++ charts/rook-release/rook-ceph/default.nix
@@ -3,4 +3,5 @@
   chart = "rook-ceph";
   version = "1.13.4";
   chartHash = "sha256-kSLZ22AKeVfgHpaO7yilNip2WVnsgcv1Ec+j940uubM=";
+  bogusVersion = true;
 }
