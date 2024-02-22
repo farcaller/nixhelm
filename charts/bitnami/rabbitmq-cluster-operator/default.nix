ee9a6b1cb4f3f510166e6f5e7b29940d2c5222ee
Aycan Irican <aycan@netskrt.io>
2024-02-22 10:51:19 -0800

    bitnami/rabbitmq-cluster-operator: init at 3.18.2

---
+++ charts/bitnami/rabbitmq-cluster-operator/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://charts.bitnami.com/bitnami/";
+  chart = "rabbitmq-cluster-operator";
+  version = "3.18.2";
+  chartHash = "sha256-mCWPyfNzhZWd4/87gCidhTWGdpt2qOOWVlmiEp2b6K0=";
+}
