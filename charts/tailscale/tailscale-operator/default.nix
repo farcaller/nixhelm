3dec15896d347ccc4568ebfec226de990544222a
Vladimir Pouzanov <farcaller@gmail.com>
2024-03-01 11:00:17 +0000

    tailscale/tailscale-operator: init at 1.61.11

---
+++ charts/tailscale/tailscale-operator/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://pkgs.tailscale.com/helmcharts/";
+  chart = "tailscale-operator";
+  version = "1.61.11";
+  chartHash = "sha256-krG4uMzdc5MO1bS6cW6RyskvWQzFWYSwCTAdNiBo1fI=";
+}
