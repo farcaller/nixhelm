704bc20378fc915b8d990aac9015d7479d1eaa4a
Vladimir Pouzanov <farcaller@gmail.com>
2024-07-05 06:49:13 +0000

    victoriametrics/victoria-metrics-k8s-stack: init at 0.23.5

---
+++ charts/victoriametrics/victoria-metrics-k8s-stack/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://victoriametrics.github.io/helm-charts/";
+  chart = "victoria-metrics-k8s-stack";
+  version = "0.23.5";
+  chartHash = "sha256-gqJxyVi9rd1Sivx7bR6m+jafUwFoa6icmDRpUQR0QfQ=";
+}
