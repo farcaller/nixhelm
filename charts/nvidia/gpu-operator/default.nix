a65acbb0ce004f2b20be415fdfbf7d9a2812f735
Vladimir Pouzanov <farcaller@gmail.com>
2024-10-28 09:41:31 +0000

    nvidia/gpu-operator: init at 24.6.2

---
+++ charts/nvidia/gpu-operator/default.nix
@@ -0,0 +1,7 @@
+{
+  repo = "https://helm.ngc.nvidia.com/nvidia/";
+  chart = "gpu-operator";
+  version = "v24.6.2";
+  chartHash = "sha256-mAjjVSMA2aGSBwxQF8FH/t2uPv6jJ+fwprv604Trxj0=";
+  bogusVersion = true;
+}
