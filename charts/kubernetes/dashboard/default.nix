86aefd087c9a42cd677440ad5c1e808a009416f3
oleina <oleina@google.com>
2025-02-07 16:56:11 -0800

    kubernetes/dashboard: init at 7.10.4

---
+++ charts/kubernetes/dashboard/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://kubernetes.github.io/dashboard/";
+  chart = "kubernetes-dashboard";
+  version = "7.10.4";
+  chartHash = "sha256-NfIPujpLUJgM5a3LaXZMlAYwHnygMtvnHFDl0tzyQUs=";
+}
