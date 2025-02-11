bb066655326a425e35218ae0108f57bb4b4ac93a
Vladimir Pouzanov <farcaller@gmail.com>
2025-02-11 17:45:46 +0000

    Remove vault-secrets-webhook (added in #6): upstream is gone.

--- charts/banzaicloud-stable/vault-secrets-webhook/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo = "https://kubernetes-charts.banzaicloud.com/";
-  chart = "vault-secrets-webhook";
-  version = "1.19.0";
-  chartHash = "sha256-yyfyrxTVMUurkco0K+QpoTtk2omx10J1ODvQERwdl+U=";
-}
