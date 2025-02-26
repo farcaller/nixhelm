f526738e34e30a4d5340aec9444f63bcbb32b407
Antonio Gurgel <git@antonio.goorzhel.com>
2025-02-25 21:33:46 -0800

    bjw-s/app-template: fix hash

--- charts/bjw-s/app-template/default.nix
+++ charts/bjw-s/app-template/default.nix
@@ -2,5 +2,5 @@
   repo = "https://bjw-s.github.io/helm-charts/";
   chart = "app-template";
   version = "3.7.1";
-  chartHash = "sha256-9ctMT0i54OnhUyUL1bPCvtYjq3LnWVyLUVn0kI07Y1E=";
+  chartHash = "sha256-z81NJ4N87CwakDsaOqCzxueJGg+r4fwG9BuHn0raWpo=";
 }
