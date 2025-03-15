89373d02edba5bdbc09d9b125867b41adbb5f676
oleina <oleina@google.com>
2025-03-14 18:23:56 -0700

    sealed-secrets/sealed-secrets: init at 2.17.1

---
+++ charts/sealed-secrets/sealed-secrets/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://bitnami-labs.github.io/sealed-secrets/";
+  chart = "sealed-secrets";
+  version = "2.17.1";
+  chartHash = "sha256-GCQAy9jePmAg5Kx/a8SblPJKM1vMdPBk72blNL2W24I=";
+}
